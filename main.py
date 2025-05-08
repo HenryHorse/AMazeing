45daad59cc9fad0909f747416b121b37ddb0ac56
Tylermhh <tyler.hlaing@gmail.com>
2025-05-08 13:30:03 -0700

    added dfs backtracker maze generator

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from models.generator import generate_random_maze
+from models.generator import generate_random_maze, generate_maze_dfs_backtracker
 from visualize import run_visualization
 
 def main():
-    num_rows, num_cols = 10, 10
-    maze = generate_random_maze(num_rows, num_cols)
+    num_rows, num_cols = 11, 11
+    maze, start, goal = generate_maze_dfs_backtracker(num_rows, num_cols)
     print(maze)
+    print(f"Start: {start}")
+    print(f"Goal: {goal}")
     run_visualization(maze, num_rows, num_cols)
 
 if __name__ == "__main__":
