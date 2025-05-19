8b4ffbcec22254c015b6a460df416c8b030d7cdc
Tylermhh <tyler.hlaing@gmail.com>
2025-05-18 17:25:56 -0700

    added prim's algorithm

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from models.generator import generate_random_maze, generate_maze_dfs_backtracker
+from models.generator import generate_random_maze, generate_maze_dfs_backtracker, prim_algo
 from visualize import run_visualization
 
 def main():
-    num_rows, num_cols = 11, 11
-    maze, start, goal = generate_maze_dfs_backtracker(num_rows, num_cols)
+    num_rows, num_cols = 21, 21
+    maze, start, goal = prim_algo(num_rows, num_cols)
     print(maze)
     print(f"Start: {start}")
     print(f"Goal: {goal}")
